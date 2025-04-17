e88a9bf521bc46bcf59576f0bda7856107f1452b
yuyu <william901008@gmail.com>
2025-04-17 13:48:32 +0800

    install ggplot

--- B124012022_期中考.R
+++ B124012022_期中考.R
@@ -3,6 +3,10 @@
 rm(list = ls())
 setwd('C:/Users/willi/OneDrive/桌面/R語言/期中考/')
 Mobiles <-  read.csv('Mobiles.csv',header=TRUE)
+install.packages('ggplot2')
+install.packages('corrplot')
+library(corrplot)
+library(ggplot2)
 
 
 #1
@@ -12,4 +16,6 @@ median(Samsung$PriceUSA);median(Apple$PriceUSA) # 699;999
 sd(Samsung$PriceUSA);sd(Apple$PriceUSA) # 515.3826;247.8969
 
 #2
+
+
 
