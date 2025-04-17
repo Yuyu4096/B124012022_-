e71a7e6046c74a8a8e4d99b157ca9032528f73b7
yuyu <william901008@gmail.com>
2025-04-17 13:47:04 +0800

    #2

--- B124012022_期中考.R
+++ B124012022_期中考.R
@@ -1,1 +1,15 @@
 #Yuyu4096
+
+rm(list = ls())
+setwd('C:/Users/willi/OneDrive/桌面/R語言/期中考/')
+Mobiles <-  read.csv('Mobiles.csv',header=TRUE)
+
+
+#1
+Samsung <- subset(Mobiles, Company=='Samsung')
+Apple <- subset(Mobiles, Company=='Apple')
+median(Samsung$PriceUSA);median(Apple$PriceUSA) # 699;999
+sd(Samsung$PriceUSA);sd(Apple$PriceUSA) # 515.3826;247.8969
+
+#2
+
