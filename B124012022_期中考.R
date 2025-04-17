d43fee66ff82daf0e30c5c37a7fc937d70bc7248
yuyu <william901008@gmail.com>
2025-04-17 13:56:50 +0800

    #3

--- B124012022_期中考.R
+++ B124012022_期中考.R
@@ -16,6 +16,17 @@ median(Samsung$PriceUSA);median(Apple$PriceUSA) # 699;999
 sd(Samsung$PriceUSA);sd(Apple$PriceUSA) # 515.3826;247.8969
 
 #2
+graph1 <- boxplot(PriceChina~Year , data = Mobiles,
+                  xlab = '年份',
+                  ylab = '中國售價',
+                  main='年份-中國售價的盒鬚圖');graph1
 
+graph2 <- hist(Mobiles$PriceChina,
+               main = '中國售價的長條圖',
+               xlab='中國售價',
+               ylab='數量',
+               col = 'gold',
+               breaks=100);graph2
 
+#3
 
